9db5d34cbeb7c4cbc2faf77ea3e79133e2adc4c4
Dhiraj9767 <dhirajbhosale7777@gmail.com>
2025-09-18 00:42:16 +0530

    browser initilizer added

--- src/test/java/com/test/BaseTest.java
+++ src/test/java/com/test/BaseTest.java
@@ -2,6 +2,7 @@ package com.test;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Properties;
 
 import com.pom.LandingPage;
@@ -9,25 +10,28 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.time.Duration;
-
 public class BaseTest {
-    public  WebDriver driver;
+    public WebDriver driver;
 
     public WebDriver initilizeDriver() throws IOException {
-
         Properties prop = new Properties();
-        FileInputStream fis = new FileInputStream(System.getProperty("user+dir") + "//src//main//java//com//resources//globalData.properties");
+
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")
+                + "//src//main//java//com//resources//globalData.properties");
         prop.load(fis);
+
         String browserName = prop.getProperty("browser");
+
         if (browserName.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
-            WebDriver driver = new ChromeDriver();
-
+            driver = new ChromeDriver();   // ✅ assign to class-level driver
         }
+        // (You can add more browsers here: firefox, edge, etc.)
+
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
         return driver;
     }
 
@@ -36,6 +40,5 @@ public class BaseTest {
         LandingPage lp = new LandingPage(driver);
         lp.goTo();
         return lp;
-
     }
 }
